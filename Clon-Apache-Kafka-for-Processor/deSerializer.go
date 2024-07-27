6d4bea127b2679afe4397e0c96595546b2a2e598
mohamadmorady412 <mohamadmorady412@gmail.com>
2024-07-27 16:20:54 +0330

    changing

--- Clon-Apache-Kafka-for-Processor/deSerializer.go
+++ Clon-Apache-Kafka-for-Processor/deSerializer.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+
+	"fmt"
+
+)
 
 func main() {
-	fmt.Println("hello world!")
+  var arr1 = [3]int{1,2,3}
+  arr2 := [...]int{4,5,6,7,8}
+
+  fmt.Println(arr1)
+  fmt.Println(arr2)
+  fmt.Println(5 ^ 6)
+
+  for idx, val := range arr2 {
+	fmt.Println("%v\t%v\n" , idx , val)
+  }
 }
